ae84359ecb020871d943919dcb0e6c200e5e1e7a
Kraig Spear <kraigspear@me.com>
2015-02-26 04:55:38 -0500

    Initial Commit

---
+++ SpearSwiftLib/SpearSwiftLib.h
@@ -0,0 +1,19 @@
+//
+//  SpearSwiftLib.h
+//  SpearSwiftLib
+//
+//  Created by Kraig Spear on 2/26/15.
+//  Copyright (c) 2015 spearware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SpearSwiftLib.
+FOUNDATION_EXPORT double SpearSwiftLibVersionNumber;
+
+//! Project version string for SpearSwiftLib.
+FOUNDATION_EXPORT const unsigned char SpearSwiftLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpearSwiftLib/PublicHeader.h>
+
+
