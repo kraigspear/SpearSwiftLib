26392fa820787a02562b0817a5c2bd7938258fa2
Kraig Spear <kraigspear@me.com>
2018-10-30 08:21:50 -0400

    Capture UIView as UIImage

--- SpearSwiftLib/UIKit/UIView+Layout.swift
+++ SpearSwiftLib/UIKit/UIView+Layout.swift
@@ -10,11 +10,27 @@ import Foundation
 
 public extension UIView {
     public func pin(to view: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+
+        if superview == nil {
+            view.addSubview(self)
+        }
+
         NSLayoutConstraint.activate([
             leadingAnchor.constraint(equalTo: view.leadingAnchor),
             trailingAnchor.constraint(equalTo: view.trailingAnchor),
             topAnchor.constraint(equalTo: view.topAnchor),
             bottomAnchor.constraint(equalTo: view.bottomAnchor),
         ])
+
+        view.layoutIfNeeded()
+    }
+
+    public func toImage() -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(bounds.size, isOpaque, 0.0)
+        defer { UIGraphicsEndImageContext() }
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        layer.render(in: context)
+        return UIGraphicsGetImageFromCurrentImageContext()
     }
 }
