5507b1b374cfe9ba03efe9c14dd8f4d94cda5543
Kraig Spear <kraigspear@me.com>
2019-04-06 12:29:25 -0400

    Tablesync

--- SpearSwiftLib/CloudKit/TableSync.swift
+++ SpearSwiftLib/CloudKit/TableSync.swift
@@ -22,10 +22,99 @@ public enum SyncError: Error {
 public typealias SyncResult = Result<Void, SyncError>
 public typealias SyncCompleted = (SyncResult) -> Void
 
+private class _AnyLocalSyncingBase<SyncRecordType>: LocalSyncing {
+	init() {
+		guard type(of: self) != _AnyLocalSyncingBase.self else {
+			fatalError("_AnyLocalSyncing<SyncRecordType> can't be created, create a subclass")
+		}
+	}
+	
+	var syncRecords: [SyncRecordType] {
+		fatalError("Override in child class")
+	}
+	
+	func updateLocalRecordsAsBeingSynched(fromCloudKit records: [CKRecord]) {
+		fatalError("Override in child class")
+	}
+	
+	func deleteLocalRecords(_ recordIDs: [CKRecord.ID]) {
+		fatalError("Override in child class")
+	}
+	
+	func deleteAll() {
+		fatalError("Override in child class")
+	}
+	
+	func saveCloudKitRecordsLocally(remoteRecordsUpdated ckRecords: [CKRecord], remoteRecordsDeleted: [CKRecord.ID]) {
+		fatalError("Override in child class")
+	}
+}
+
+private final class _AnyLocalSyncingBox<Concrete: LocalSyncing>: _AnyLocalSyncingBase<Concrete.SyncRecordType> {
+	var concrete: Concrete
+	init(_ concrete: Concrete) {
+		self.concrete = concrete
+	}
+	
+	override var syncRecords: [Concrete.SyncRecordType] {
+		return self.concrete.syncRecords
+	}
+	
+	override func updateLocalRecordsAsBeingSynched(fromCloudKit records: [CKRecord]) {
+		concrete.updateLocalRecordsAsBeingSynched(fromCloudKit: records)
+	}
+	
+	override func deleteLocalRecords(_ recordIDs: [CKRecord.ID]) {
+		concrete.deleteLocalRecords(recordIDs)
+	}
+	
+	override func deleteAll() {
+		concrete.deleteAll()
+	}
+	
+	override func saveCloudKitRecordsLocally(remoteRecordsUpdated ckRecords: [CKRecord], remoteRecordsDeleted: [CKRecord.ID]) {
+		concrete.saveCloudKitRecordsLocally(remoteRecordsUpdated: ckRecords,
+		                                    remoteRecordsDeleted: remoteRecordsDeleted)
+	}
+}
+
+public final class AnyLocalSyncing<SyncRecordType>: LocalSyncing {
+	private let box: _AnyLocalSyncingBase<SyncRecordType>
+	
+	public init<Concrete: LocalSyncing>(_ concrete: Concrete) where Concrete.SyncRecordType == SyncRecordType {
+		self.box = _AnyLocalSyncingBox(concrete)
+	}
+	
+	public var syncRecords: [SyncRecordType] {
+		return box.syncRecords
+	}
+	
+	public func updateLocalRecordsAsBeingSynched(fromCloudKit records: [CKRecord]) {
+		box.updateLocalRecordsAsBeingSynched(fromCloudKit: records)
+	}
+	
+	public func deleteLocalRecords(_ recordIDs: [CKRecord.ID]) {
+		box.deleteLocalRecords(recordIDs)
+	}
+	
+	public func deleteAll() {
+		box.deleteAll()
+	}
+	
+	public func saveCloudKitRecordsLocally(remoteRecordsUpdated ckRecords: [CKRecord], remoteRecordsDeleted: [CKRecord.ID]) {
+		box.saveCloudKitRecordsLocally(remoteRecordsUpdated: ckRecords,
+		                               remoteRecordsDeleted: remoteRecordsDeleted)
+	}
+}
+
 /**
  Local datastore capabibilites related to Syncing
  */
 public protocol LocalSyncing {
+	associatedtype SyncRecordType
+	
+	var syncRecords: [SyncRecordType] { get }
+	
 	/**
 	 Changes have been saved to CloudKit
 	 Mark the local records as being synched
@@ -61,10 +150,10 @@ public enum SyncDirection {
 open class TableSyncBase<SyncRecordType> {
 	private let log = SwiftyBeaver.self
 	
-	private let localSync: LocalSyncing
+	private let localSync: AnyLocalSyncing<SyncRecordType>
 	private let queue: OperationQueue
 	
-	public init(localSync: LocalSyncing) {
+	public init(localSync: AnyLocalSyncing<SyncRecordType>) {
 		self.localSync = localSync
 		queue = OperationQueue()
 		queue.maxConcurrentOperationCount = 1
@@ -114,6 +203,7 @@ open class TableSyncBase<SyncRecordType> {
 	}
 	
 	private final class SyncToOperation<SyncRecordType>: BaseOperation {
+		private let log = SwiftyBeaver.self
 		private let toCloudKit: SyncToCloudKit<SyncRecordType>
 		
 		private(set) var syncResult: SyncResult?
@@ -123,12 +213,17 @@ open class TableSyncBase<SyncRecordType> {
 		}
 		
 		override func main() {
+			log.info("SyncToOperation.main")
+			
 			guard isCancelled == false else {
+				log.info("SyncToOperation was cancelled")
 				done()
 				return
 			}
 			
+			log.info("Calling toCloudKit")
 			toCloudKit.sync { [weak self] result in
+				self?.log.info("toCloudKit completed")
 				self?.syncResult = result
 				self?.done()
 			}
@@ -181,6 +276,8 @@ open class TableSyncBase<SyncRecordType> {
 	 */
 	public func sync(direction: SyncDirection,
 	                 completed: @escaping SyncCompleted) {
+		log.info("sync direction: \(direction)")
+		
 		switch direction {
 		case .fromCouldKit:
 			
@@ -199,14 +296,16 @@ open class TableSyncBase<SyncRecordType> {
 			
 			let toOperation = SyncToOperation(toCloudKit: toCloudKit)
 			
-			toOperation.completionBlock = {
+			toOperation.completionBlock = { [weak self] in
+				self?.log.info("SyncToOperation: completed")
+				
 				if let result = toOperation.syncResult {
 					DispatchQueue.main.async {
 						completed(result)
 					}
 				}
 			}
-			
+			log.info("SyncToOperation adding operation")
 			queue.addOperation(toOperation)
 		case .both:
 			
@@ -255,6 +354,7 @@ open class TableSyncBase<SyncRecordType> {
 	}
 	
 	fileprivate final class ChangeTokens {
+		private let log = SwiftyBeaver.self
 		private let defaults = UserDefaults.standard
 		
 		private let zonesChangedInDatabaseKey = "databaseChangeKey"
@@ -291,22 +391,30 @@ open class TableSyncBase<SyncRecordType> {
 		
 		var recordChangeToken: CKServerChangeToken? {
 			get {
+				log.debug("get recordChangeToken key: \(recordChangeTokenKey)")
+				
 				guard let data = defaults.data(forKey: recordChangeTokenKey) else {
+					log.debug("\(recordChangeTokenKey) is nil, returing nil")
 					return nil
 				}
 				
 				do {
-					return try NSKeyedUnarchiver.unarchivedObject(ofClass: CKServerChangeToken.self, from: data)
-				} catch {
+					let changeToken = try NSKeyedUnarchiver.unarchivedObject(ofClass: CKServerChangeToken.self, from: data)
+					log.debug("Succesfully saved recordChangeToken to \(recordChangeTokenKey)")
+					return changeToken
+				} catch let error {
+					log.error("Can't unarchive recordChangeToken?: \(error)")
 					assertionFailure("Can't unarchive recordChangeToken?")
 					return nil
 				}
 			}
 			set {
 				if let token = newValue {
+					log.debug("set recordChangeToken key: \(recordChangeTokenKey)")
 					let data = try! NSKeyedArchiver.archivedData(withRootObject: token, requiringSecureCoding: false)
 					defaults.set(data, forKey: recordChangeTokenKey)
 				} else {
+					log.debug("set recordChangeToken key: \(recordChangeTokenKey) to nil")
 					defaults.setValue(nil, forKey: recordChangeTokenKey)
 				}
 			}
@@ -318,14 +426,13 @@ open class TableSyncBase<SyncRecordType> {
 		}
 	}
 	
-	public final class SyncToCloudKit<SyncRecordType> {
+	final class SyncToCloudKit<SyncRecordType> {
 		private let log = SwiftyBeaver.self
-		private var completed: SyncCompleted!
 		private weak var tableSync: TableSyncBase<SyncRecordType>?
-		private let localSyncing: LocalSyncing
+		private let localSyncing: AnyLocalSyncing<SyncRecordType>
 		
 		init(tableSync: TableSyncBase<SyncRecordType>,
-		     localSyncing: LocalSyncing) {
+		     localSyncing: AnyLocalSyncing<SyncRecordType>) {
 			self.tableSync = tableSync
 			self.localSyncing = localSyncing
 		}
@@ -333,25 +440,25 @@ open class TableSyncBase<SyncRecordType> {
 		public func sync(completed: @escaping SyncCompleted) {
 			log.info("SyncToCloudKit starting")
 			
-			assert(self.completed == nil, "completed already assigned")
-			self.completed = completed
-			
 			CloudKitAvailable.check { [weak self] isAvailable in
 				
+				guard let self = self else { return }
+				
 				guard isAvailable else {
-					self?.log.info("CloudKit is not enabled")
-					DispatchQueue.main.async {
-						completed(SyncResult.failure(.cloudKitNotEnabled))
-					}
+					self.log.info("CloudKit is not enabled")
+					completed(SyncResult.failure(.cloudKitNotEnabled))
 					return
 				}
 				
-				self?.log.info("CloudKit is avilable")
-				self?.syncUpCloudKitAvailable()
+				self.log.info("CloudKit is available")
+				self.syncUpCloudKitAvailable(completed: completed)
 			}
 		}
 		
-		private func syncUpCloudKitAvailable() {
+		/**
+		 Sync up after it's been confirmed that CloudKit is available
+		 */
+		private func syncUpCloudKitAvailable(completed: @escaping SyncCompleted) {
 			log.info("syncUpCloudKitAvailable")
 			guard let tableSync = self.tableSync else {
 				log.warning("tableSync is nil, not syncing")
@@ -362,10 +469,7 @@ open class TableSyncBase<SyncRecordType> {
 			
 			guard recordsToSyncToCloudKit.isEmpty == false else {
 				log.info("recordsToSyncToCloudKit is empty, nothing to update")
-				
-				DispatchQueue.main.async {
-					self.completed(SyncResult.success(()))
-				}
+				completed(SyncResult.success(()))
 				return
 			}
 			
@@ -374,18 +478,23 @@ open class TableSyncBase<SyncRecordType> {
 				
 				switch result {
 				case let .failure(error):
-					self?.completed(.failure(error))
+					completed(.failure(error))
 				case .success:
-					self?.syncUpAfterConfirmedThatRecordZoneExist()
+					self?.syncUpAfterConfirmedThatRecordZoneExist(completed: completed)
 				}
 			}
 		}
 		
-		private func syncUpAfterConfirmedThatRecordZoneExist() {
+		private func syncUpAfterConfirmedThatRecordZoneExist(completed: @escaping SyncCompleted) {
+			log.info("syncUpAfterConfirmedThatRecordZoneExist")
+			
 			guard let tableSync = self.tableSync else { return }
 			
 			let ckRecordsToUpdate = tableSync.ckRecordsToUpdate
 			let ckRecordIdsToDelete = tableSync.ckRecordIdsToDelete
+			
+			log.debug("ckRecordsToUpdate.count: \(ckRecordsToUpdate.count)")
+			log.debug("ckRecordIdsToDelete.count: \(ckRecordIdsToDelete.count)")
 			
 			let modifyOperation = CKModifyRecordsOperation(recordsToSave: ckRecordsToUpdate,
 			                                               recordIDsToDelete: ckRecordIdsToDelete.count > 0 ? ckRecordIdsToDelete : nil)
@@ -399,11 +508,11 @@ open class TableSyncBase<SyncRecordType> {
 				
 				if let error = error {
 					self.log.error("Error updating CloudKit: \(error)")
-					DispatchQueue.main.async {
-						self.completed(Result.failure(.cloudKitSaveFailed))
-					}
+					completed(Result.failure(.cloudKitSaveFailed))
 					return
 				}
+				
+				self.log.info("Success updating CloudKit")
 				
 				if let updated = updated {
 					self.localSyncing.updateLocalRecordsAsBeingSynched(fromCloudKit: updated)
@@ -412,8 +521,11 @@ open class TableSyncBase<SyncRecordType> {
 				if let deletedRecordIds = deletedRecordIds {
 					self.localSyncing.deleteLocalRecords(deletedRecordIds)
 				}
+				
+				completed(Result.success(()))
 			}
 			
+			log.info("modifyOperation.run")
 			modifyOperation.run()
 		}
 	}
@@ -422,11 +534,11 @@ open class TableSyncBase<SyncRecordType> {
 		private let log = SwiftyBeaver.self
 		private var completed: SyncCompleted!
 		private weak var tableSync: TableSyncBase<SyncRecordType>?
-		private let localSyncing: LocalSyncing
+		private let localSyncing: AnyLocalSyncing<SyncRecordType>
 		private let changeTokens: ChangeTokens
 		
 		fileprivate init(tableSync: TableSyncBase<SyncRecordType>,
-		                 localSyncing: LocalSyncing,
+		                 localSyncing: AnyLocalSyncing<SyncRecordType>,
 		                 changeTokens: ChangeTokens) {
 			self.tableSync = tableSync
 			self.localSyncing = localSyncing
@@ -434,25 +546,30 @@ open class TableSyncBase<SyncRecordType> {
 		}
 		
 		public func sync(completed: @escaping SyncCompleted) {
+			log.info("SyncFromCloudKit")
+			
 			guard let tableSync = self.tableSync else {
 				log.error("tableSync is nil")
 				preconditionFailure("tableSync is nil")
 			}
 			
+			log.info("fetchRecordZonesChanged")
 			tableSync.recordZone.fetchRecordZonesChanged { [weak self] result in
 				
 				guard let self = self else { return }
 				
 				switch result {
 				case let .failure(error):
+					self.log.error("Error fetchRecordZonesChanged: \(error)")
 					completed(SyncResult.failure(error))
 				case let .success(recordZoneChange):
 					
 					if recordZoneChange.didChange {
-						// Our recordZone has changed.
+						self.log.info("Our recordZone has changed, updating")
 						self.updateRecords(completed: completed)
 						
 					} else {
+						self.log.info("RecordZone has not changed, return success")
 						completed(SyncResult.success(()))
 					}
 				}
@@ -461,6 +578,8 @@ open class TableSyncBase<SyncRecordType> {
 		
 		typealias FetchRecordsToUpdateResult = (Result<Int, Error>)
 		private func updateRecords(completed: @escaping SyncCompleted) {
+			log.info("FromCloudKit: updateRecords")
+			
 			guard let tableSync = self.tableSync else {
 				log.error("tableSync is nil")
 				preconditionFailure("tableSync is nil")
@@ -470,9 +589,18 @@ open class TableSyncBase<SyncRecordType> {
 			                                                                     resultsLimit: nil,
 			                                                                     desiredKeys: nil)
 			
+//			let zoneConfig = CKFetchRecordZoneChangesOperation.ZoneConfiguration(previousServerChangeToken: nil,
+//			                                                                     resultsLimit: nil,
+//			                                                                     desiredKeys: nil)
+			
 			let recordZoneId = tableSync.recordZoneId
 			
 			let config: [CKRecordZone.ID: CKFetchRecordZoneChangesOperation.ZoneConfiguration] = [recordZoneId: zoneConfig]
+			
+			log.debug("""
+			CKFetchRecordZoneChangesOperation
+			recordZoneId: \(recordZoneId)
+			""")
 			
 			let fetchRecordZoneChangesOperation = CKFetchRecordZoneChangesOperation(recordZoneIDs: [recordZoneId],
 			                                                                        configurationsByRecordZoneID: config)
@@ -483,16 +611,22 @@ open class TableSyncBase<SyncRecordType> {
 			var recordIDsToDelete: [CKRecord.ID] = []
 			var newRecordChangeToken: CKServerChangeToken?
 			
-			fetchRecordZoneChangesOperation.recordChangedBlock = { record in
+			fetchRecordZoneChangesOperation.recordChangedBlock = { [weak self] record in
+				self?.log.debug("recordChangedBlock: \(record)")
 				recordsToUpdate.append(record)
 			}
 			
-			fetchRecordZoneChangesOperation.recordWithIDWasDeletedBlock = { recordId, _ in
+			fetchRecordZoneChangesOperation.recordWithIDWasDeletedBlock = { [weak self] recordId, _ in
+				self?.log.debug("recordWithIDWasDeletedBlock: \(recordId)")
 				recordIDsToDelete.append(recordId)
 			}
 			
-			fetchRecordZoneChangesOperation.recordZoneFetchCompletionBlock = { _, recordChangeToken, _, moreComing, error in
+			fetchRecordZoneChangesOperation.recordZoneFetchCompletionBlock = { [weak self] _, recordChangeToken, _, moreComing, error in
+				
+				self?.log.debug("recordZoneFetchCompletionBlock: changeToken \(String(describing: recordChangeToken)) moreComing: \(moreComing)")
+				
 				if moreComing == false && error == nil {
+					self?.log.debug("moreComing false, no error assigning token")
 					newRecordChangeToken = recordChangeToken
 				}
 			}
@@ -500,6 +634,8 @@ open class TableSyncBase<SyncRecordType> {
 			fetchRecordZoneChangesOperation.fetchRecordZoneChangesCompletionBlock = { [weak self] error in
 				
 				guard let self = self else { return }
+				
+				self.log.info("fetchRecordZoneChangesCompletionBlock")
 				
 				if let error = error {
 					if let ckError = error as? CKError {
@@ -522,6 +658,7 @@ open class TableSyncBase<SyncRecordType> {
 				}
 				
 				if let newRecordChangeToken = newRecordChangeToken {
+					self.log.debug("newChangeToken, updating records")
 					self.updateRecords(toBeUpdated: recordsToUpdate,
 					                   toBeDeleted: recordIDsToDelete,
 					                   changeToken: newRecordChangeToken,
@@ -533,6 +670,7 @@ open class TableSyncBase<SyncRecordType> {
 				}
 			}
 			
+			log.info("Running: fetchRecordZoneChangesOperation")
 			fetchRecordZoneChangesOperation.run()
 		}
 		
@@ -540,12 +678,21 @@ open class TableSyncBase<SyncRecordType> {
 		                           toBeDeleted: [CKRecord.ID],
 		                           changeToken: CKServerChangeToken,
 		                           completed: @escaping SyncCompleted) {
+			log.info("""
+			SyncFromCloudKit: updateRecords
+			toBeUpdated: \(toBeUpdated.count)
+			toBeDeleted: \(toBeDeleted.count)
+			changeToken: \(changeToken)
+			""")
+			
 			localSyncing.saveCloudKitRecordsLocally(remoteRecordsUpdated: toBeUpdated,
 			                                        remoteRecordsDeleted: toBeDeleted)
 			
 			// Changes saved locally. we can not mark these changes as being accounted for by
 			// saving the changeToken. Next time we will changes beyond this change token
 			self.changeTokens.recordChangeToken = changeToken
+			
+			completed(Result.success(()))
 		}
 	}
 	
@@ -709,6 +856,8 @@ open class TableSyncBase<SyncRecordType> {
 		}
 	}
 }
+
+extension TableSyncBase: NameDescribable {}
 
 private extension CKDatabaseOperation {
 	func run() {
