6bd1bfa5718641ebba22a8f0d319f87c1febb42c
Kraig Spear <kraigspear@me.com>
2020-12-13 09:12:46 -0500

    Update

--- Package.swift
+++ Package.swift
@@ -1,13 +1,26 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
     name: "SpearSwiftLib",
     products: [
-        .library(name: "SpearSwiftLib", targets: ["SwiftyBeaver"]),
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "SpearSwiftLib",
+            targets: ["SpearSwiftLib"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        .target(name: "SwiftyBeaver", dependencies: [], path: "Sources"),
-        .testTarget(name: "SwiftyBeaverTests", dependencies: ["SwiftyBeaver"]),
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "SpearSwiftLib",
+            dependencies: []),
+        .testTarget(
+            name: "SpearSwiftLibTest",
+            dependencies: ["SpearSwiftLib"]),
     ]
 )
